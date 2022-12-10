8da64288947a776ac5ba0041de16ba9d0239b600
Rohman <rohmanpendo@gmail.com>
2022-12-10 18:01:30 +0700

    style: apply styled component to detail page

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -1,9 +1,12 @@
+import Container from "../../components/styled/Container";
+import Image from "../../components/styled/Image";
+
 export default function Detail({ restaurant }) {
   const { name, description, pictureId, menus: { foods, drinks } } = restaurant;
 
   return (
-    <div className="container">
-      <img src={`https://restaurant-api.dicoding.dev/images/large/${pictureId}`} alt={name} />
+    <Container maxWidth="88%">
+      <Image src={`https://restaurant-api.dicoding.dev/images/large/${pictureId}`} alt={name} height="512px" />
 
       <header>
         <h1>{name}</h1>
@@ -29,7 +32,7 @@ export default function Detail({ restaurant }) {
           ))}
         </ul>
       </main>
-    </div>
+    </Container>
   )
 }
 
