b695e687a0e77d4c3b6e4a18c12ac14f8d25b66d
Rohman <rohmanpendo@gmail.com>
2022-12-10 17:41:38 +0700

    feat: add Container styled component

---
+++ components/styled/Container.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 1rem;
+  max-width: ${(props) => props.maxWidth};
+  margin: ${(props) => props.margin};
+`;
+
+Container.defaultProps = {
+  maxWidth: "100%",
+  margin: "0 auto"
+}
+
+export default Container;
